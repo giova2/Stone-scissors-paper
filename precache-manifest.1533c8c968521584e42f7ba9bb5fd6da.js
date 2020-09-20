4a63f4b1ca4ebe3b1e3cdfd519f6317a1947a950
franco giovannangelo <fcgiova@gmail.com>
2020-09-20 00:29:06 -0300

    Updates

---
+++ precache-manifest.1533c8c968521584e42f7ba9bb5fd6da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7b3e1bf6af720ad2af017d7946f51e",
+    "url": "/Stone-scissors-paper/Stone-scissors-paper/index.html"
+  },
+  {
+    "revision": "0b8df7461da112768e20",
+    "url": "/Stone-scissors-paper/Stone-scissors-paper/static/css/main.8990ec86.chunk.css"
+  },
+  {
+    "revision": "1c361f246634abe019ba",
+    "url": "/Stone-scissors-paper/Stone-scissors-paper/static/js/2.0d2485f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stone-scissors-paper/Stone-scissors-paper/static/js/2.0d2485f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8df7461da112768e20",
+    "url": "/Stone-scissors-paper/Stone-scissors-paper/static/js/main.58446fe5.chunk.js"
+  },
+  {
+    "revision": "b3e5c0fee1541de8ae7f",
+    "url": "/Stone-scissors-paper/Stone-scissors-paper/static/js/runtime-main.9990c783.js"
+  }
+]);
